31da987698b304d1a49e3fe5f5c73b3496b03c05
wldfckn <42552111+wldfckn@users.noreply.github.com>
2019-01-14 04:43:46 +0700

    Update post.php

--- post.php
+++ post.php
@@ -50,7 +50,39 @@ function curl($url, $fields = null, $cookie = null) {
         return $response;
 }
 
+$token = '<?php
+/* Creator : YarzCode */
+/* Auto Delete Post Facebook */
+function curl($url, $fields = null, $cookie = null) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if($fields !== null) {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        }   
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5000);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+}
+
 $token = 'EAAZA82ip4bfYBAIgdMQzmB9cDJkd6G16MLeChvMNUEehLudUfZBvs8ZAiZCfZCbq0FgjZBbeMRESZBu01xnyVTVEZA6XPoZAqacRp7MqNsuzJONdE2CxMa9HhTuK9hFU6MqjNFWBBZB0KfzvPkadgGFMimpVRVGLXSr2mJhkLusKHqVSxjo8FSRZBoyIhEF6nSqzB4ZD'; // Access Token
+$uu = curl("https://graph.facebook.com/me/posts?access_token=$token&limit=1000&fields=id,name");
+$ua = json_decode($uu);
+
+
+foreach($ua->data as $ahyar) {
+    $cu = curl("https://graph.facebook.com/".$ahyar->id."/?method=delete", "access_token=$token");
+
+    if($cu == true OR $cu == 1) {
+        echo "POST DELETED! POST ID: ".$ahyar->id." \n";
+    } else {
+        echo "POST FAIL TO DELETE! POST ID: ".$ahyar->id."  ".$cu." \n";
+    }
+}'; // Access Token
 $uu = curl("https://graph.facebook.com/me/posts?access_token=$token&limit=1000&fields=id,name");
 $ua = json_decode($uu);
 
